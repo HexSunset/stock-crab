231454f6fbc39e20168720cd75f0669f0e52de15
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2022-06-21 01:31:55 +0300

    Added or_assign method for bitboards

--- src/position/bitboard.rs
+++ src/position/bitboard.rs
@@ -1,25 +1,36 @@
 #![allow(dead_code)]
 
+#[derive(Clone, Copy)]
 pub struct BitBoard(u64);
 
 impl BitBoard {
-    fn toggle(&mut self, x: usize, y: usize) -> Result<(), ()> {
+    pub fn new() -> BitBoard {
+        BitBoard(0)
+    }
+
+    pub fn as_u64(&self) -> u64 {
+        self.0
+    }
+
+    pub fn toggle(&mut self, x: u32, y: u32) {
         if y <= 7 && x <= 7 {
             let mask: u64 = 1 << (y * 8 + x);
             self.0 ^= mask;
-            Ok(())
         } else {
-            Err(())
         }
     }
 
-    fn get(&self, x: usize, y: usize) -> Result<bool, ()> {
+    pub fn get(&self, x: usize, y: usize) -> Option<bool> {
         if x <= 7 && y <= 7 {
             let mask: u64 = 1 << (y * 8 + x);
-            Ok((self.0 | mask) != 0)
+            Some((self.0 | mask) != 0)
         } else {
-            Err(())
+            None
         }
+    }
+
+    pub fn or_assign(&mut self, b: BitBoard) {
+        self.0 |= b.as_u64();
     }
 }
 
@@ -30,18 +41,18 @@ mod test {
     #[test]
     fn bitboard() {
         let mut bb1 = BitBoard(0);
-        assert!(bb1.toggle(0, 0).is_ok());
-        assert!(bb1.0 == 1);
+        bb1.toggle(0, 0);
+        assert!(bb1.as_u64() == 1);
 
-        assert!(bb1.toggle(0, 0).is_ok());
-        assert!(bb1.0 == 0);
+        bb1.toggle(0, 0);
+        assert!(bb1.as_u64() == 0);
 
-        assert!(bb1.toggle(8, 0).is_err());
+        bb1.toggle(8, 0);
 
-        assert!(bb1.toggle(7, 7).is_ok());
-        assert!(bb1.0 == 9223372036854775808);
+        bb1.toggle(7, 7);
+        assert!(bb1.as_u64() == 9223372036854775808);
 
-        assert!(bb1.toggle(7, 7).is_ok());
-        assert!(bb1.0 == 0);
+        bb1.toggle(7, 7);
+        assert!(bb1.as_u64() == 0);
     }
 }
